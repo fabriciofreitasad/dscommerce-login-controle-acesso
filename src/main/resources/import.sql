a0319c92d3715e3c6537ab24fbe84092bf5c615e
fabricio freitas <fabricio.freitas.ad@gmail.com>
2023-08-17 12:46:24 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$jsuWmsPW8pSdhR2NWTocYecjg0iP2ZF/LrsDi0HuPhZjVJqqnD2fm');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$jsuWmsPW8pSdhR2NWTocYecjg0iP2ZF/LrsDi0HuPhZjVJqqnD2fm');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
