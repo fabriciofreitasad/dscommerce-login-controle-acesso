10f34072e6ba9d053114c311c57ada3a18f845fa
fabricio freitas <fabricio.freitas.ad@gmail.com>
2023-08-22 12:04:06 -0300

    GET user logged

--- src/main/java/com/devsuperior/demo/services/UserService.java
+++ src/main/java/com/devsuperior/demo/services/UserService.java
@@ -3,11 +3,16 @@ package com.devsuperior.demo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.demo.dto.UserDTO;
 import com.devsuperior.demo.entities.Role;
 import com.devsuperior.demo.entities.User;
 import com.devsuperior.demo.projections.UserDetailsProjection;
@@ -15,25 +20,43 @@ import com.devsuperior.demo.repositories.UserRepository;
 
 @Service
 public class UserService implements UserDetailsService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+
 		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
-		
-		if(result.size()==0) {
+
+		if (result.size() == 0) {
 			throw new UsernameNotFoundException("User not found");
 		}
 		User user = new User();
 		user.setEmail(username);
 		user.setPassword(result.get(0).getPassword());
-		for(UserDetailsProjection projection : result) {
-			user.addRole(new Role(projection.getRoleId(),projection.getAuthority()));
+		for (UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
 		}
 		return user;
 	}
+	
+	protected User authenticated() {
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+			String username = jwtPrincipal.getClaim("username");
 
+			return  repository.findByEmail(username).get();
+		}
+		catch(Exception e) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		
+	}
+	 @Transactional(readOnly = true)
+	public UserDTO getMe() {
+		User user = authenticated();
+		return new UserDTO(user);
+	}
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$jsuWmsPW8pSdhR2NWTocYecjg0iP2ZF/LrsDi0HuPhZjVJqqnD2fm');
 INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$jsuWmsPW8pSdhR2NWTocYecjg0iP2ZF/LrsDi0HuPhZjVJqqnD2fm');
 
-INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
